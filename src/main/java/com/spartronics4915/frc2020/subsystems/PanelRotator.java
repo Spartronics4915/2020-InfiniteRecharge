f3e58164c350aa536ac50cc04219b1b36a7952d3
Noah Solomon <crashtest64@micahsolomon>
2020-01-17 17:24:02 -0800

    created methods for the Panel Rotator

--- src/main/java/com/spartronics4915/frc2020/subsystems/PanelRotator.java
+++ src/main/java/com/spartronics4915/frc2020/subsystems/PanelRotator.java
@@ -1,1 +1,19 @@
 // I'm bad at naming things. Please come up with a better name...
+package com.spartronics4915.frc2020.subsystems;
+
+import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
+
+public class PanelRotator extends SpartronicsSubsystem {
+    public PanelRotator(){
+        
+    }
+    public void rotate(number){
+
+    }
+    public String testColor(){
+
+    }
+    public void stop(){
+
+    }
+}
