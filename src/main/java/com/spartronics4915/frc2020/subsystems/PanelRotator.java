cf2ce97eb1604b6ab65091a64189e45669be80ba
Noah Solomon <crashtest64@micahsolomon>
2020-01-17 17:39:34 -0800

    fixed problems—made number defined (as a double to allow partial rotations), let Justice add some spaces

--- src/main/java/com/spartronics4915/frc2020/subsystems/PanelRotator.java
+++ src/main/java/com/spartronics4915/frc2020/subsystems/PanelRotator.java
@@ -4,15 +4,19 @@ package com.spartronics4915.frc2020.subsystems;
 import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
 
 public class PanelRotator extends SpartronicsSubsystem {
+
     public PanelRotator(){
         
     }
-    public void rotate(number){
+
+    public void rotate(double number){
 
     }
-    public String testColor(){
+
+    public String color(){
 
     }
+
     public void stop(){
 
     }
