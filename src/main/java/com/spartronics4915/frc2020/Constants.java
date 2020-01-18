fb7ff8e3dcb7efbb6f5927498c707abb5c21c027
Noah Solomon <crashtest64@micahsolomon>
2020-01-17 19:46:06 -0800

    added two constants, used a sensor and a motor

--- src/main/java/com/spartronics4915/frc2020/Constants.java
+++ src/main/java/com/spartronics4915/frc2020/Constants.java
@@ -1,4 +1,6 @@
 package com.spartronics4915.frc2020;
 
 public final class Constants {
+    public static final int kBeamSensorID = -1;
+    public static final int kPanelMotorID = -1;
 }
--- src/main/java/com/spartronics4915/frc2020/subsystems/PanelRotator.java
+++ src/main/java/com/spartronics4915/frc2020/subsystems/PanelRotator.java
@@ -1,16 +1,25 @@
 // I'm bad at naming things. Please come up with a better name...
 package com.spartronics4915.frc2020.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.spartronics4915.frc2020.Constants;
 import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public class PanelRotator extends SpartronicsSubsystem {
+    private final CANSparkMax mPanelMotor;
+    private final DigitalInput mBeamSensor;
 
     public PanelRotator() {
-        
+        mBeamSensor = new DigitalInput(Constants.kBeamSensorID);
+        mPanelMotor = new CANSparkMax(Constants.kPanelMotorID, MotorType.kBrushless);
     }
 
     public void rotate(double number) {
-
+        if(mBeamSensor.get())
+            mPanelMotor.set(0);
     }
 
     public String color() {
