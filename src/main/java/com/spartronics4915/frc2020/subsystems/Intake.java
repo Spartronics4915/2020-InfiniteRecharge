9b409223b9ea3027fcf71a451b48c56f34aa4a17
L1f3GuOrD <tuohyfre000@frogrock.org>
2020-01-18 13:42:06 -0800

    I added the API for the methods in the intake subsustem

---
+++ src/main/java/com/spartronics4915/frc2020/subsystems/Intake.java
@@ -0,0 +1,22 @@
+package com.spartronics4915.frc2020.subsystems;
+
+import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
+
+public class Intake extends SpartronicsSubsystem {
+
+    public Intake() {
+
+    }
+    
+    public static void intake() {
+
+    }
+
+    public static void reverse() {
+
+    }
+
+    public static void stop() {
+
+    }
+}
