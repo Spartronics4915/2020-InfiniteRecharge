07dc32d77e4cc5dc4987b48fe9a40ae5ead63347
L1f3GuOrD <tuohyfre000@frogrock.org>
2020-01-18 15:47:33 -0800

    I added a comment explaining the subsystem and its methods.

--- src/main/java/com/spartronics4915/frc2020/subsystems/Intake.java
+++ src/main/java/com/spartronics4915/frc2020/subsystems/Intake.java
@@ -2,6 +2,29 @@ package com.spartronics4915.frc2020.subsystems;
 
 import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
 
+/** The Intake subsystem takes balls from 
+ * the playing field and outputs them to storage
+ * 
+ * mechvecintake: activates the mechanum 
+ * "vector" wheels in partial intake, hopefully
+ * in tandem with the prism roller
+ * 
+ * prisrollintake: activates the prism roller
+ * in partial intake, hopefully in tandem with
+ * the mechanum "vector" wheels
+ * 
+ * prisrollreverse: reverses the prism roller
+ * 
+ * mechvecreverse: reverses the mechanum
+ * wheels
+ * 
+ * prisrollstop: stops the prism roller
+ * 
+ * mechvecstop: stops the mechanum wheels
+ * 
+ * stop: a universal stop method
+ * 
+ */
 public class Intake extends SpartronicsSubsystem {
 
     public Intake() {
