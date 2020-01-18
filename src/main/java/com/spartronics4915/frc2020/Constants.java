3d48229907594b7d47949c1723cda4ea1f617c25
Noah Solomon <crashtest64@micahsolomon>
2020-01-18 15:11:42 -0800

    color sensors output, but are not used well

--- src/main/java/com/spartronics4915/frc2020/Constants.java
+++ src/main/java/com/spartronics4915/frc2020/Constants.java
@@ -4,12 +4,16 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.I2C.Port;
 
 public final class Constants {
-    public static final int kBeamSensorUpID = -1;
-    public static final int kBeamSensorDownID = -1;
+    public static final int kPanelRotatorBeamSensorUpID = -1;
+    public static final int kPanelRotatorBeamSensorDownID = -1;
 
-    public static final int kExtendMotorID = -1;
-    public static final int kSpinMotorID = -1;
+    public static final int kPanelRotatorExtendMotorID = -1;
+    public static final int kPanelRotatorSpinMotorID = -1;
 
-    public static final Port kColorSensorLeftID = I2C.Port.kOnboard;
-    public static final int kColorSensorRightID =-1;
+    //TODO:This is bad and does not work. Needs to be implemented in a functional way.
+    public static final Port kPanelRotatorColorSensorLeftID = I2C.Port.kOnboard;
+    public static final Port kPanelRotatorColorSensorRightID = I2C.Port.kOnboard;
+
+    public static final double kPanelRotatorExtendMotorSpeed = 0.5;
+    public static final double kPanelRotatorSpinMotorSpeed = 0.5;
 }
--- src/main/java/com/spartronics4915/frc2020/subsystems/PanelRotator.java
+++ src/main/java/com/spartronics4915/frc2020/subsystems/PanelRotator.java
@@ -6,47 +6,57 @@ import com.revrobotics.ColorSensorV3;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.spartronics4915.frc2020.Constants;
 import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
-
-import 
-
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class PanelRotator extends SpartronicsSubsystem {
     private final CANSparkMax mSpinMotor;
     private final CANSparkMax mExtendMotor;
+    
     private final DigitalInput mBeamSensorUp;
     private final DigitalInput mBeamSensorDown;
-    private final ColorSensorV3 mColorSensorRight;
-    private final ColorSensorV3 mColorSensorLeft;
-    private final I2C.Port i2cPort = 
+    
+    private final ColorSensorV3  mColorSensor;
+
+    private int[] mMinimumRed = {255, 0, 0};
+    private int[] mMaximumRed = {255, 0, 0};  
+    
+    public int red;
+    public int green;
+    public int blue;
 
     public PanelRotator() {
-        mBeamSensorUp = new DigitalInput(Constants.kBeamSensorUpID);
-        mBeamSensorDown = new DigitalInput(Constants.kBeamSensorDownID);
-        mSpinMotor = new CANSparkMax(Constants.kSpinMotorID, MotorType.kBrushless);
-        mExtendMotor = new CANSparkMax(Constants.kExtendMotorID, MotorType.kBrushless);
-        mColorSensorLeft = new ColorSensorV3(Constants.kColorSensorLeftID);
-        mColorSensorRight = new ColorSensorV3(Constants.kColorSensorRightID);
+        mBeamSensorUp = new DigitalInput(Constants.kPanelRotatorBeamSensorUpID);
+        mBeamSensorDown = new DigitalInput(Constants.kPanelRotatorBeamSensorDownID);
+        mSpinMotor = new CANSparkMax(Constants.kPanelRotatorSpinMotorID, MotorType.kBrushless);
+        mExtendMotor = new CANSparkMax(Constants.kPanelRotatorExtendMotorID, MotorType.kBrushless);
+        mColorSensor = new ColorSensorV3(I2C.Port.kOnboard);
     }
 
     public void raise() {
-        mExtendMotor.set(0.5);
+        mExtendMotor.set(Constants.kPanelRotatorExtendMotorSpeed);
         if(!mBeamSensorUp.get())
             mExtendMotor.set(0);
     }
+
     public void lower() {
-        mExtendMotor.set(-0.5);
+        mExtendMotor.set(-Constants.kPanelRotatorExtendMotorSpeed);
         if(mBeamSensorDown.get())
             mExtendMotor.set(0);
     }
 
-    public void spin(double speed) {
-       
+    public String getTargetColor() {
+        String color = DriverStation.getInstance().getGameSpecificMessage();
+        return color;
     }
 
-    public String color() {
-        return "red but that's a lie";
-    }
+    /*public String getClosestColor() {
+        red =  mColorSensor.getRed();
+        green =  mColorSensor.getGreen();
+        blue =  mColorSensor.getBlue();
+        
+    }*/
 
     public void stop() {
 
