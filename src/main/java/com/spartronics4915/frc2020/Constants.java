390948e49c5e7ba72a7247451c710f816ffe71e2
Noah Solomon <crashtest64@micahsolomon>
2020-01-17 20:31:54 -0800

    ok this does not work; however Justice wants me to push my code

--- src/main/java/com/spartronics4915/frc2020/Constants.java
+++ src/main/java/com/spartronics4915/frc2020/Constants.java
@@ -1,6 +1,15 @@
 package com.spartronics4915.frc2020;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
+
 public final class Constants {
-    public static final int kBeamSensorID = -1;
-    public static final int kPanelMotorID = -1;
+    public static final int kBeamSensorUpID = -1;
+    public static final int kBeamSensorDownID = -1;
+
+    public static final int kExtendMotorID = -1;
+    public static final int kSpinMotorID = -1;
+
+    public static final Port kColorSensorLeftID = I2C.Port.kOnboard;
+    public static final int kColorSensorRightID =-1;
 }
--- src/main/java/com/spartronics4915/frc2020/subsystems/PanelRotator.java
+++ src/main/java/com/spartronics4915/frc2020/subsystems/PanelRotator.java
@@ -2,24 +2,46 @@
 package com.spartronics4915.frc2020.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorSensorV3;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.spartronics4915.frc2020.Constants;
 import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
 
+import 
+
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class PanelRotator extends SpartronicsSubsystem {
-    private final CANSparkMax mPanelMotor;
-    private final DigitalInput mBeamSensor;
+    private final CANSparkMax mSpinMotor;
+    private final CANSparkMax mExtendMotor;
+    private final DigitalInput mBeamSensorUp;
+    private final DigitalInput mBeamSensorDown;
+    private final ColorSensorV3 mColorSensorRight;
+    private final ColorSensorV3 mColorSensorLeft;
+    private final I2C.Port i2cPort = 
 
     public PanelRotator() {
-        mBeamSensor = new DigitalInput(Constants.kBeamSensorID);
-        mPanelMotor = new CANSparkMax(Constants.kPanelMotorID, MotorType.kBrushless);
+        mBeamSensorUp = new DigitalInput(Constants.kBeamSensorUpID);
+        mBeamSensorDown = new DigitalInput(Constants.kBeamSensorDownID);
+        mSpinMotor = new CANSparkMax(Constants.kSpinMotorID, MotorType.kBrushless);
+        mExtendMotor = new CANSparkMax(Constants.kExtendMotorID, MotorType.kBrushless);
+        mColorSensorLeft = new ColorSensorV3(Constants.kColorSensorLeftID);
+        mColorSensorRight = new ColorSensorV3(Constants.kColorSensorRightID);
     }
 
-    public void rotate(double number) {
-        if(mBeamSensor.get())
-            mPanelMotor.set(0);
+    public void raise() {
+        mExtendMotor.set(0.5);
+        if(!mBeamSensorUp.get())
+            mExtendMotor.set(0);
+    }
+    public void lower() {
+        mExtendMotor.set(-0.5);
+        if(mBeamSensorDown.get())
+            mExtendMotor.set(0);
+    }
+
+    public void spin(double speed) {
+       
     }
 
     public String color() {
