1cbcdc9802a657fc5dcf4708b2448a789646331c
AustinThomasSmith <austinthomassmith2002@gmail.com>
2020-01-18 13:10:27 -0800

    Made the variables finals

--- src/main/java/com/spartronics4915/frc2020/Constants.java
+++ src/main/java/com/spartronics4915/frc2020/Constants.java
@@ -1,6 +1,6 @@
 package com.spartronics4915.frc2020;
 
 public final class Constants {
-    public static int kClimber775ProID = 5;
-    public static int kClimberNEOID = 6;
+    public static final int kClimber775ProID = 5;
+    public static final int kClimberNEOID = 6;
 }
