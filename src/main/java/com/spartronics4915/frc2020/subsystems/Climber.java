d8af560ec83ddd22c745a70aa6a8c8f43199f244
AustinThomasSmith <austinthomassmith2002@gmail.com>
2020-01-17 17:29:48 -0800

    Added method names for the climber (no motors yet, will add when gotten)

---
+++ src/main/java/com/spartronics4915/frc2020/subsystems/Climber.java
@@ -0,0 +1,28 @@
+package com.spartronics4915.frc2020.subsystems;
+
+import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
+
+public class Climber extends SpartronicsSubsystem {
+
+    public Climber() {
+        //Hardware Contructor (Add motors and such here when I get them)
+        
+    }
+    
+    public static void extend() {
+        
+    }
+
+    public static void winch(){
+
+    }
+
+    public static void reverse(){
+
+    }
+
+    public static void stop(){
+
+    }
+
+}
