f882c3d8409338291a688e61e42891c1a62ca136
Camden Greenhalgh <lego76666@gmail.com>
2020-01-17 17:37:46 -0800

    Wrote Indexer Stub

--- src/main/java/com/spartronics4915/frc2020/subsystems/ExampleSubsystem.java
+++ src/main/java/com/spartronics4915/frc2020/subsystems/ExampleSubsystem.java
@@ -8,10 +8,20 @@ public class ExampleSubsystem extends SpartronicsSubsystem {
         // Construct your hardware here
     }
 
+    @Override
+    public void outputTelemetry() {
+        // TODO Auto-generated method stub
+
+    }
+
     // Outline your API here by creating specific methods.
     // Each method should perform a _singular action_
     // - eg. instead of a setIntake method, control each intake motor individually
     // setIntake functionality should be implemented in a command.
+
+    public boolean ballLoaded() {
+        return false;
+    }
 
     // The exception to this is a general-functionality stop() method.
 }
---
+++ src/main/java/com/spartronics4915/frc2020/subsystems/Indexer.java
@@ -0,0 +1,72 @@
+package com.spartronics4915.frc2020.subsystems;
+
+import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
+
+public class Indexer extends SpartronicsSubsystem {
+
+    public Indexer() {
+        // Construct your hardware here
+    }
+
+    // Outline your API here by creating specific methods.
+    // Each method should perform a _singular action_
+    // - eg. instead of a setIntake method, control each intake motor individually
+    // setIntake functionality should be implemented in a command.
+
+    public boolean getBallLoaded() 
+    {
+        return false;
+    }
+
+    public void rotateClockwiseOnce() 
+    {
+
+    }
+
+    public void rotateCounterClockwiseOnce() 
+    {
+
+    }
+
+    public int rotateCounterClockwiseFull() {
+        return 0;
+    }
+
+    public int rotateClockwiseFull() {
+        return 0;
+    }
+
+    public int rotateFull()
+    {
+        return 0;
+    }
+
+    public int rotateCounterClockwiseEmpty() {
+        return 0;
+    }
+
+    public int rotateClockwiseEmpty() {
+        return 0;
+    }
+
+    public int rotateEmpty() {
+        return 0;
+    }
+
+    public void stop() 
+    {
+
+    }
+
+    public void liftUp()
+    {
+
+    }
+
+    public void liftDown()
+    {
+
+    }
+
+    // The exception to this is a general-functionality stop() method.
+}
