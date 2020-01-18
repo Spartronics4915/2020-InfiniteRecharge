adab921b707fa042099ef86752e5b5729cb66e1a
AustinThomasSmith <austinthomassmith2002@gmail.com>
2020-01-17 20:22:41 -0800

    Added method scaffolds and motor ids in constants

--- src/main/java/com/spartronics4915/frc2020/subsystems/Climber.java
+++ src/main/java/com/spartronics4915/frc2020/subsystems/Climber.java
@@ -1,36 +1,43 @@
 package com.spartronics4915.frc2020.subsystems;
 
 import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
+import com.spartronics4915.frc2020.Constants;
 import com.spartronics4915.lib.hardware.motors.SpartronicsSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Climber extends SpartronicsSubsystem {
 
-    private TalonSRX mClimber775PRO;
-    private CANSparkMax mClimberNEO;
+    private static TalonSRX mClimber775PRO;
+    private static CANSparkMax mClimberNEO;
     public Climber() {
         //Hardware Contructor (Add motors and such here when I get them)
-        mClimber775PRO = new TalonSRX(5);
-        mClimberNEO = new CANSparkMax(6, MotorType.kBrushless);
+        mClimber775PRO = new TalonSRX(Constants.kClimber775ProID);
+        mClimberNEO = new CANSparkMax(Constants.kClimberNEOID, MotorType.kBrushless);
         
     }
     
     public static void extend() {
-        
+        mClimber775PRO.set(ControlMode.PercentOutput, 0.0);
+        mClimberNEO.set(0.0);
     }
 
     public static void winch() {
-
+        mClimber775PRO.set(ControlMode.PercentOutput, 0.0);
+        mClimberNEO.set(0.0);
     }
 
     public static void reverse() {
-
+        mClimber775PRO.set(ControlMode.PercentOutput, 0.0);
+        mClimberNEO.set(0.0);
     }
 
     public static void stop() {
-
+        mClimber775PRO.set(ControlMode.PercentOutput, 0.0);
+        mClimberNEO.set(0.0);
     }
 
 }
