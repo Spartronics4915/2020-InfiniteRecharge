2b9dbe6d37b2ef7e60270d4365eb7ebf9ebcfa00
j-james <jj@j-james.me>
2020-01-21 21:34:07 -0800

    Rename SpinToColor SpinToColorCommand

--- src/main/java/com/spartronics4915/frc2020/commands/SpinToColorCommand.java
+++ src/main/java/com/spartronics4915/frc2020/commands/SpinToColorCommand.java
@@ -4,10 +4,10 @@ import com.spartronics4915.frc2020.subsystems.PanelRotator;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class SpinToColor extends CommandBase {
+public class SpinToColorCommand extends CommandBase {
 
     // You should only use one subsystem per command. If multiple are needed, use a CommandGroup.
-    public SpinToColor(PanelRotator mSubsystem) {
+    public SpinToColorCommand(PanelRotator mSubsystem) {
 
     }
 
@@ -20,7 +20,7 @@ public class SpinToColor extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        
+
     }
 
     // Returns true when the command should end.
