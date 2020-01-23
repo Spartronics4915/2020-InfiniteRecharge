0d4233c63d6aea8ab61a2ee9cb94d43b98d005fa
Noah Solomon <crashtest64@micahsolomon>
2020-01-22 16:50:30 -0800

    warning when pulling said to commit first

--- src/main/java/com/spartronics4915/frc2020/commands/SpinToColor.java
+++ src/main/java/com/spartronics4915/frc2020/commands/SpinToColor.java
@@ -2,6 +2,7 @@ package com.spartronics4915.frc2020.commands;
 
 import com.spartronics4915.frc2020.subsystems.PanelRotator;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class SpinToColor extends CommandBase {
@@ -14,13 +15,31 @@ public class SpinToColor extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        
+        String gameData;
+        gameData = DriverStation.getInstance().getGameSpecificMessage();
+        switch (gameData.charAt(0))
+        {
+          case 'B' :
+            PanelRotator.spin(0.1);
+            break;
+          case 'G' :
+            //Green case code
+            break;
+          case 'R' :
+            //Red case code
+            break;
+          case 'Y' :
+            //Yellow case code
+            break;
+          default :
+            //This is corrupt data
+            break;
+        }
     }
 
     // Returns true when the command should end.
