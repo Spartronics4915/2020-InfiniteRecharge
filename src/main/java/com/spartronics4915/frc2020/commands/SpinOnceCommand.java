f8d1fc15902dfe42503fb1f005a56d13a5a0228a
Noah Solomon <crashtest64@micahsolomon>
2020-01-24 15:51:20 -0800

    auto formatting

--- src/main/java/com/spartronics4915/frc2020/commands/SpinOnceCommand.java
+++ src/main/java/com/spartronics4915/frc2020/commands/SpinOnceCommand.java
@@ -9,44 +9,46 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class SpinOnceCommand extends CommandBase {
 
-    private PanelRotator mPanelRotator;
-    public double spins;
-    public String firstColor;
-    public String lastColor;
-    public String currentColor;
-    // You should only use one subsystem per command. If multiple are needed, use a CommandGroup.
-    public SpinOnceCommand(PanelRotator mSubsystem) {
-      mPanelRotator = mSubsystem;
-    }
+  private PanelRotator mPanelRotator;
+  public double spins;
+  public String firstColor;
+  public String lastColor;
+  public String currentColor;
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
-      spins = 0;
-      firstColor = mPanelRotator.getActualColor();
-    }
+  // You should only use one subsystem per command. If multiple are needed, use a
+  // CommandGroup.
+  public SpinOnceCommand(PanelRotator mSubsystem) {
+    mPanelRotator = mSubsystem;
+  }
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-      currentColor = mPanelRotator.getActualColor();
-      if(currentColor == firstColor && currentColor != lastColor)
-        spins += .5;
-      lastColor = mPanelRotator.getActualColor();
-    }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    spins = 0;
+    firstColor = mPanelRotator.getActualColor();
+  }
 
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-      if(spins == 1)
-        return true;
-      else
-        return false;
-    }
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    currentColor = mPanelRotator.getActualColor();
+    if (currentColor == firstColor && currentColor != lastColor)
+      spins += .5;
+    lastColor = mPanelRotator.getActualColor();
+  }
 
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end (boolean interrupted) {
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if (spins == 1)
+      return true;
+    else
+      return false;
+  }
 
-    }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+  }
 }
