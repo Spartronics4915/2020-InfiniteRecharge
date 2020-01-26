5370293e8ee5896383cc4723f5b107ffce1dc99c
j-james <jj@j-james.me>
2020-01-26 13:57:42 -0800

    Fix indentation

--- src/main/java/com/spartronics4915/frc2020/commands/SpinToColorCommand.java
+++ src/main/java/com/spartronics4915/frc2020/commands/SpinToColorCommand.java
@@ -8,71 +8,71 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class SpinToColorCommand extends CommandBase
 {
 
-  private PanelRotator mPanelRotator;
+    private PanelRotator mPanelRotator;
 
-  // You should only use one subsystem per command. If multiple are needed, use a
-  // CommandGroup.
-  public SpinToColorCommand(PanelRotator mSubsystem)
-  {
-    mPanelRotator = mSubsystem;
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize()
-  {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute()
-  {
-    String targetColor = mPanelRotator.getTargetColor();
-    targetColor = DriverStation.getInstance().getGameSpecificMessage();
-    switch (targetColor.charAt(0))
+    // You should only use one subsystem per command. If multiple are needed, use a
+    // CommandGroup.
+    public SpinToColorCommand(PanelRotator mSubsystem)
     {
-      case 'B':
-        if (mPanelRotator.getActualColor() != "Blue")
-        {
-          mPanelRotator.spin();
-        }
-        break;
-      case 'G':
-        if (mPanelRotator.getActualColor() != "Green")
-        {
-          mPanelRotator.spin();
-        }
-        break;
-      case 'R':
-        if (mPanelRotator.getActualColor() != "Red")
-        {
-          mPanelRotator.spin();
-        }
-        break;
-      case 'Y':
-        if (mPanelRotator.getActualColor() != "Yellow")
-        {
-          mPanelRotator.spin();
-        }
-        break;
-      default:
-        // This is corrupt data
-        break;
+        mPanelRotator = mSubsystem;
     }
 
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize()
+    {
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished()
-  {
-    return false;
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute()
+    {
+        String targetColor = mPanelRotator.getTargetColor();
+        targetColor = DriverStation.getInstance().getGameSpecificMessage();
+        switch (targetColor.charAt(0))
+        {
+            case 'B':
+                if (mPanelRotator.getActualColor() != "Blue")
+                {
+                  mPanelRotator.spin();
+                }
+                break;
+            case 'G':
+                if (mPanelRotator.getActualColor() != "Green")
+                {
+                  mPanelRotator.spin();
+                }
+                break;
+            case 'R':
+                if (mPanelRotator.getActualColor() != "Red")
+                {
+                  mPanelRotator.spin();
+                }
+                break;
+            case 'Y':
+                if (mPanelRotator.getActualColor() != "Yellow")
+                {
+                  mPanelRotator.spin();
+                }
+                break;
+            default:
+                // This is corrupt data
+                break;
+        }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted)
-  {
+    }
 
-  }
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished()
+    {
+        return false;
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted)
+    {
+
+    }
 }
