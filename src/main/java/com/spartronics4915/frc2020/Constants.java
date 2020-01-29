2c975cb19b22e83fbb36c30fc26ee8cae73693ef
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2020-01-29 15:49:38 -0800

    Add switchable constants and logInitialized to drive

--- src/main/java/com/spartronics4915/frc2020/Constants.java
+++ src/main/java/com/spartronics4915/frc2020/Constants.java
@@ -1,5 +1,12 @@
 package com.spartronics4915.frc2020;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.spartronics4915.lib.util.Logger;
+
 public final class Constants {
 
     public static final class Climber {
@@ -9,12 +16,12 @@ public final class Constants {
         public static final double kWinchSpeed = 1.0;
         public static final boolean kStalled = true;
     }
-    
+
     public static final class Indexer {
         public static final int kSpinnerId = -1;
         public static final int kLoaderId = -1;
         public static final int kProxSensorId = -1;
-    }   
+    }
 
     public static final class Launcher {
         public static final int kFlywheelMasterID = -1;
@@ -37,5 +44,26 @@ public final class Constants {
 
         public static final double kExtendMotorSpeed = 0.5;
         public static final double kSpinMotorSpeed = 0.5;
+    }
+
+    // Initialize blank fields that are robot-specific here
+    static {
+        String config = "default";
+        Path machineIDPath = FileSystems.getDefault().getPath(System.getProperty("user.home"), "machineid");
+        try {
+            config = Files.readString(machineIDPath).trim().toLowerCase();
+        } catch (IOException e) {
+        }
+        Logger.notice("Running on " + config + " constants");
+
+        switch (config) {
+            case "test chassis":
+                // Put constants here
+                break;
+            case "default":
+            case "real robot":
+                // Or here
+                break;
+        }
     }
 }
