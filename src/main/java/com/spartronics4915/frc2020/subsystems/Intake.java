32eb1cadf92582eb3364aa3ae01dc559a19defc8
L1f3GuOrD <tuohyfre000@frogrock.org>
2020-01-29 17:01:01 -0800

    Updated doc comments and names. Added new method that checks to see if there's a ball in the chamber.

--- src/main/java/com/spartronics4915/frc2020/subsystems/Intake.java
+++ src/main/java/com/spartronics4915/frc2020/subsystems/Intake.java
@@ -5,24 +5,27 @@ import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
 /** The Intake subsystem takes balls from 
  * the playing field and outputs them to storage
  * 
- * mechvecintake: activates the mechanum 
+ * harvestintake: activates the mechanum 
  * "vector" wheels in partial intake, hopefully
  * in tandem with the prism roller
  * 
- * prisrollintake: activates the prism roller
+ * ingestintake: activates the prism roller
  * in partial intake, hopefully in tandem with
  * the mechanum "vector" wheels
  * 
- * prisrollreverse: reverses the prism roller
+ * ingestreverse: reverses the prism roller
  * 
- * mechvecreverse: reverses the mechanum
+ * harvestreverse: reverses the mechanum
  * wheels
  * 
- * prisrollstop: stops the prism roller
+ * ingeststop: stops the prism roller
  * 
- * mechvecstop: stops the mechanum wheels
+ * harveststop: stops the mechanum wheels
+ * 
+ * isballheld: checks to see if there's a ball in the intake chamber
  * 
  * stop: a universal stop method
+ * 
  * 
  */
 public class Intake extends SpartronicsSubsystem {
