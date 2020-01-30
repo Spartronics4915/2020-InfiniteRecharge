b8268ff54a42e5939b8ec1967acfea19252aad82
AustinThomasSmith <austinthomassmith2002@gmail.com>
2020-01-29 17:46:46 -0800

    Finished adding inline commands into the ClimberCommands.java file

--- src/main/java/com/spartronics4915/frc2020/commands/ClimberCommands.java
+++ src/main/java/com/spartronics4915/frc2020/commands/ClimberCommands.java
@@ -3,8 +3,10 @@ package com.spartronics4915.frc2020.commands;
 import javax.xml.namespace.QName;
 
 import com.spartronics4915.frc2020.subsystems.Climber;
+import com.spartronics4915.frc2020.Constants;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 
 public class ClimberCommands
@@ -33,8 +35,22 @@ public class ClimberCommands
         }
     }
 
-    public class Winch extends CommandBase
+    public class WinchPrimary extends FunctionalCommand
     {
-public Winch()
+        public WinchPrimary(Climber mClimber)
+        {
+            super(() ->
+            {
+            }, () -> mClimber.winch(!Constants.Climber.kStalled), (Boolean b) -> mClimber.stop(),
+                    mClimber::isStalled, mClimber);
+        }
+    }
+
+    public class WinchSecondary extends StartEndCommand
+    {
+        public WinchSecondary(Climber mClimber)
+        {
+            super(() -> mClimber.winch(Constants.Climber.kStalled), mClimber::stop, mClimber);
+        }
     }
 }
