74b86fb36bb648ea6c78d23cbef4175ac4e643e9
Noah Solomon <crashtest64@micahsolomon>
2020-02-05 20:00:58 -0800

    I think that that is the right id?

--- src/main/java/com/spartronics4915/frc2020/Constants.java
+++ src/main/java/com/spartronics4915/frc2020/Constants.java
@@ -84,7 +84,7 @@ public final class Constants
 
     public static final class PanelRotator
     {
-        public static final int kBeamSensorUpId = 2; // A2 // Optical Flag for Zeroing
+        public static final int kBeamSensorUpId = 5;
         public static final boolean kBeamSensorUpReversed = false; // Whether or not the beam sensor is inverted
 
         public static final int kLimitSwitchDownId = -1;
