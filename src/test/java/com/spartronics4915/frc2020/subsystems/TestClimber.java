a7a0090ada7ab3d39637def93e527c1a293e4991
j-james <35242550+j-james@users.noreply.github.com>
2020-02-06 01:38:20 -0800

    Update TestClimber.java

--- src/test/java/com/spartronics4915/frc2020/subsystems/TestClimber.java
+++ src/test/java/com/spartronics4915/frc2020/subsystems/TestClimber.java
@@ -1,4 +1,5 @@
-/*package com.spartronics4915.frc2020.subsystems;
+/*
+package com.spartronics4915.frc2020.subsystems;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -28,4 +29,5 @@ public class TestClimber
         simmedMotor.setOutputCurrent(11);
         assertFalse(commandToRun.isScheduled());
     }
-}*/
+}
+*/
