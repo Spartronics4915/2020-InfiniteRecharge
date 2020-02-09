dd0ae2e7e98fde5824520700dc6e5852debad171
Dana Batali <dana.batali@gmail.com>
2020-02-08 16:10:38 -0800

    Renamed test suite.

--- src/test/java/com/spartronics4915/lib/math/threedim/AffineTests.java
+++ src/test/java/com/spartronics4915/lib/math/threedim/AffineTests.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class TransformationsTest
+class AffineTests
 {
     double kEpsilon = 1e-9;
 
