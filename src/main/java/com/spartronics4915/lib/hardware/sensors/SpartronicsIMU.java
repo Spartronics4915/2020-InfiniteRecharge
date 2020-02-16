2737205d2e9586d94a581b685a25ef67b5477f34
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2020-02-16 11:27:43 -0800

    Lib refactoring

--- src/main/java/com/spartronics4915/lib/hardware/sensors/SpartronicsIMU.java
+++
@@ -1,8 +0,0 @@
-package com.spartronics4915.lib.hardware.sensors;
-
-import com.spartronics4915.lib.math.twodim.geometry.Rotation2d;
-
-public interface SpartronicsIMU
-{
-    Rotation2d getYaw();
-}
