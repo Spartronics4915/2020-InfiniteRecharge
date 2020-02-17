88690a8d6494f52526d5de59a1b2ae342fd9ed19
Randy Groves <randomgrace@gmail.com>
2020-02-17 13:43:04 -0800

    Add initialize message to instance creation

--- src/main/java/com/spartronics4915/frc2020/subsystems/LED.java
+++ src/main/java/com/spartronics4915/frc2020/subsystems/LED.java
@@ -1,6 +1,7 @@
 package com.spartronics4915.frc2020.subsystems;
 
 import com.spartronics4915.lib.subsystems.SpartronicsSubsystem;
+import com.spartronics4915.lib.util.Logger;
 
 import java.util.Arrays;
 import com.fazecast.jSerialComm.SerialPort;
@@ -52,6 +53,7 @@ public class LED extends SpartronicsSubsystem
             mBlingPort.openPort();
 
             logInitialized(true);
+			Logger.notice("LED: Initialized serial port");
         }
         catch (Exception e)
         {
