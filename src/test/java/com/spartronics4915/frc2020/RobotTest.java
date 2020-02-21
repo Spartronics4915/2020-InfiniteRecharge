cafe5b6bb2998356e3528bd722c7c8475e5cbdbf
Dana Batali <dana.batali@gmail.com>
2020-02-21 10:30:25 -0800

    New test method initRobot(). (disabled)

--- src/test/java/com/spartronics4915/frc2020/RobotTest.java
+++ src/test/java/com/spartronics4915/frc2020/RobotTest.java
@@ -11,8 +11,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * a simple test of basic robot init.  This turns out to be quite
  * slippery in the context of the HALJni configuration.  We apparently
- * can't expect to run this interactively.  It is also very sensitive
- * to static construction sequence, etc.  I landed at this point
+ * can't expect to run this interactively but the thread below provides some
+ * vscode settings that I've verified as functiona.  It appears to be very 
+ * sensitive to static construction sequence, etc.  I landed at this point
  * by search-and-destroy methods.   Hopefully we can learn more
  * about the inherent subtlties.
  * 
@@ -31,7 +32,7 @@ class RobotTest
     RobotTest()
     {
         sSim = new DriverStationSim();
-        //sSim.setAutonomous(false);
+        //sSim.setAutonomous(false); // these cause a crash
         //sSim.setEnabled(true);
     }
 
@@ -39,5 +40,21 @@ class RobotTest
     public void initTest()
     {
         sSubsys.logNotice("initTest found this config: " + Constants.sConfig);        
+    }
+
+    /* this test, when run interactively is sorta interesting, we
+     * disable it until such time as we can determine how to request exit.
+     * Issues that a manual run points out:
+     *      - Built by: null  on: null  (null) (even though Manifest looks fine)
+     *      - missing native libspartronicsnative.so
+     *      - missing USB/serial throws an error
+     *      - CTR: CAN frame not received/too-stale.
+     *      - Joystick Button 1 on port 0 not available, check if controller is plugged in
+    */
+    // @Test
+    public void initRobot()
+    {
+        RobotBase.startRobot(Robot::new);
+        sSim.setEnabled(false);
     }
 }
