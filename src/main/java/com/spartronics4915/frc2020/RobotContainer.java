cb938526b5de7f5039ed32bd948af9d4ab1fb97d
Dana Batali <dana.batali@gmail.com>
2020-02-22 16:21:12 -0800

    Cleanup inputs.

--- src/main/java/com/spartronics4915/frc2020/RobotContainer.java
+++ src/main/java/com/spartronics4915/frc2020/RobotContainer.java
@@ -31,12 +31,10 @@ import com.spartronics4915.lib.util.Logger;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public class RobotContainer
