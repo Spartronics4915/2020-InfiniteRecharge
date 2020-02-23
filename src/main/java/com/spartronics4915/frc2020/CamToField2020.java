1620be7c77d8f0c1bc96935d105353433f5e2e01
loqoman <darwin.s.clark@gmail.com>
2020-02-22 17:14:51 -0800

    Adding measured mounting intrensics

--- src/main/java/com/spartronics4915/frc2020/CamToField2020.java
+++ src/main/java/com/spartronics4915/frc2020/CamToField2020.java
@@ -17,13 +17,13 @@ import com.spartronics4915.lib.math.twodim.geometry.Rotation2d;
  * x, y and z (z for robot origin is assumed 0 the floor).  Turret may 
  * also be rotated to point opposite to the robot front.
  * 
- *                       y
- *                 ._____|____. 
- *                 |     |    |             
+ *                   y   y
+ *                 ._|___|____. 
+ *              x--|-t   |    |             
  *                 |     o-----x
- *              x--- T        |
- *                 |_|________| 
- *                   y
+ *                 |          |
+ *                 |__________| 
+ *                   
  * 
  * Turret - x coord points to center of shooting.  Turret angle of 0
  * means shoot straight. We expect a range of turret angles from -45 to 45 deg.
@@ -60,26 +60,27 @@ public class CamToField2020 extends CameraToField
     // camera to mount -----------------------------------------------------
     // camTilt represents the amount the camera is pointed up, measured in
     // degrees around camera's x axis. To measure it, point the camera at
-    // a wall from a perpendicular orientation.  Mark the center of the 
-    // camera with a mark.  Measure the distance from height of the mark
-    // to the camera origin.  Measure the distance from the wall.  Use
+    // a wall from a perpendicular orientation. Mark the center of the
+    // camera with a mark. Measure the distance from height of the mark
+    // to the camera origin. Measure the distance from the wall. Use
     // degrees(atan2(mark, dist)) to compute the tilt angle.
-    private final double mCamTilt = 15; // if you change this also change
+    private final double mCamTilt = 20; // if you change this also change
 
     // camPos represents offset of camera's origin relative to turret origin.
-    // (measured in inches).  Camera origin is the center of the focal point.
+    // (measured in inches). Camera origin is the center of the focal point.
     // So we do expect some z offset as well as x.
-    private final Vec3 mCamPos = new Vec3(0, -12, 0); 
+    private final Vec3 mCamPos = new Vec3(8, 0, 2.25);
 
     // camFlips maps camera axes to mount axes. eg: cam x is turret -y (above)
-    private final Vec3 mCamFlipX = new Vec3(0, -1, 0);  // cam +x is turret's -y
-    private final Vec3 mCamFlipY = new Vec3(0, 0, 1);   // cam +y is turret's +z
-    private final Vec3 mCamFlipZ = new Vec3(-1, 0, 0);  // cam +z is turret's -x
+    private final Vec3 mCamFlipX = new Vec3(0, -1, 0); // cam +x is turret's -y
+    private final Vec3 mCamFlipY = new Vec3(0, 0, 1); // cam +y is turret's +z
+    private final Vec3 mCamFlipZ = new Vec3(-1, 0, 0); // cam +z is turret's -x
 
     // mount to robot ------------------------------------------------------
     // turret is mounted at robot back/center, up from ground
-    // turret's x axis is opposite robot's 
-    private final Vec3 mMntPos = new Vec3(-15, 0, 8); 
+    // turret's x axis is opposite robot's
+
+    private final Vec3 mMntPos = new Vec3(-3.72, 5.264, 15.625);
     private final Vec3 mMntAxis = Vec3.ZAxis;
     private final Affine3 mMntFlip = Affine3.fromRotation(180, this.mMntAxis);
 
