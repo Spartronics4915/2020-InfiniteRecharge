fade422fe01df320549e5bd66bd8a845a8f73288
j-james <jj@j-james.me>
2021-06-27 20:39:19 -0700

    Decrease spindexer speed and increase kicker wait time

--- src/main/java/com/spartronics4915/frc2020/Constants.java
+++ src/main/java/com/spartronics4915/frc2020/Constants.java
@@ -71,8 +71,8 @@ public final class Constants
             public static final double kPositionP = 0.005;
             public static final double kPositionD = 0;
             public static final double kConversionRatio = 1.0 / (187.0/20.0*9.0);
-            public static final double kMaxVelocity = 3.5; // These values (3.5 and 2.5) have been extensively tested
-            public static final double kMaxAcceleration = 2.5;
+            public static final double kMaxVelocity = 2.0;
+            public static final double kMaxAcceleration = 1.0;
             public static final double kStallThreshold = 10.0;
             public static final double kMaxUnjamTime = 1.0;
 
